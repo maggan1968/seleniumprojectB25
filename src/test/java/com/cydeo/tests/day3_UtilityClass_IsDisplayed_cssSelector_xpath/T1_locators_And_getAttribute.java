874cfd8302c4d9eb34962cf95ac92d23a90d4960
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-02-08 12:28:30 -0600

    day3 created getText GetAttribute

--- src/test/java/com/cydeo/tests/day3_UtilityClass_IsDisplayed_cssSelector_xpath/T1_locators_And_getAttribute.java
+++ src/test/java/com/cydeo/tests/day3_UtilityClass_IsDisplayed_cssSelector_xpath/T1_locators_And_getAttribute.java
@@ -1,12 +1,11 @@
 package com.cydeo.tests.day3_UtilityClass_IsDisplayed_cssSelector_xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class locators_And_getAttribute {
+public class T1_locators_And_getAttribute {
     /*
     TC #1: NextBaseCRM, locators and getText() practice
 1- Open a Chrome browser
@@ -25,8 +24,10 @@ elements.
 
         //TC #1: NextBaseCRM, locators and getText() practice
         //1- Open a Chrome browser
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
+       // WebDriverManager.chromedriver().setup();
+        //WebDriver driver = new ChromeDriver();
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
 
         //2- Go to: https://login1.nextbasecrm.com/
