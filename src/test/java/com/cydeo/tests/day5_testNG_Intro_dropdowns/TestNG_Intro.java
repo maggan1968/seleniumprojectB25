f86f71cc78ee995dab9753db0f5bf3cab64361cb
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-02-15 15:11:45 -0600

    day5 TestNG_Intro

---
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,54 @@
+package com.cydeo.tests.day5_testNG_Intro_dropdowns;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestNG_Intro {
+
+
+
+    @Test (priority = 1)
+    public void test1(){
+
+        System.out.println("Test 1 is running...");
+
+        //ASSERT EQUALS:compare 2 of the same things
+        String actual ="apple";
+        String expected = "apple";
+        Assert.assertEquals("actual", "actual");
+    }
+
+    @Test (priority = 2)
+    public void test2(){
+        System.out.println("Test 2 is running...");
+        //ASSERT TRUE
+        String actual ="apple";
+        String expected = "apple";
+
+        Assert.assertTrue(actual.equals(expected));
+    }
+
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("-----> BeforeClass is running!");
+
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("-----> AfterClass is running!");
+
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---> BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---> AfterMethod is running!");
+    }
+
+
+}
