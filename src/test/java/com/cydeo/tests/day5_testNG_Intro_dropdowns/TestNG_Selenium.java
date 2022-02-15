27a47eeea8fb14707d7a96f8511e04fb3c0e7d05
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-02-15 15:13:27 -0600

    day5 TestNG_Selenium

---
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_dropdowns/TestNG_Selenium.java
@@ -0,0 +1,45 @@
+package com.cydeo.tests.day5_testNG_Intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNG_Selenium {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //do browser driver setup
+        // open browser
+        driver = WebDriverFactory.getDriver("Chrome");
+
+        //maximize the page
+        driver.manage().window().maximize();
+
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.close();
+}
+    @Test
+    public void selenium_test(){
+
+        //get "https://google.com"
+        driver.get("https://google.com");
+
+       //Assert" title is "Google"
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Google";
+       // this way ----> Assert.assertEquals(driver.getTitle(), "Google");
+       //OR
+        Assert.assertEquals(actualTitle, expectedTitle, "Title is not matching here");
+    }
+}
