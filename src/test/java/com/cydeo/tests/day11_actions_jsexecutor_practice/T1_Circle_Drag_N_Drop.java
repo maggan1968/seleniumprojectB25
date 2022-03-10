04d7c63b68035aa9d1a8a68b359eb1a3b5397390
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-03-10 12:03:34 -0600

    day11  T1_Circle_Drag_N_Drop

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
@@ -1,9 +1,11 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T1_Circle_Drag_N_Drop {
@@ -25,11 +27,21 @@ public class T1_Circle_Drag_N_Drop {
         WebElement bigCircle = Driver.getDriver().findElement(By.id("droptarget"));
 
 //2. Drag and drop the small circle to bigger circle.
+        BrowserUtils.sleep(2);
         Actions actions = new Actions(Driver.getDriver());
-        actions.dragAndDrop(smallCircle, bigCircle).perform();
+        //actions.dragAndDrop(smallCircle, bigCircle).perform();
+        actions.clickAndHold(smallCircle)
+                .pause(2000)
+                .moveToElement(bigCircle)
+                .pause(2000)
+                .release()
+                .perform();
 
 //3. Assert:
-//            -
+        String actualBigCircleText = bigCircle.getText();
+        String expectedBigCircleText = "You did great!";
+
+        Assert.assertEquals(actualBigCircleText, expectedBigCircleText);
 //  Text in big circle changed to: “You did great!”
 
 
