b20eed79e39235c14dee0d700a9820b53dfd241e
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-03-11 16:03:27 -0600

    day12  POM Practice

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractice.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractice.java
@@ -4,11 +4,18 @@ import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
 import com.github.javafaker.Faker;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class POMPractice {
 
     LibraryLoginPage libraryLoginPage;
+
+    @BeforeMethod
+    public void setupMethod() {
+        Driver.getDriver().get("https://library1.cydeo.com");
+        libraryLoginPage = new LibraryLoginPage();
+    }
 
     @Test
     public void Required_field_error_message_test() {
@@ -17,10 +24,9 @@ public class POMPractice {
 //    TC #1: Required field error message test
 //1- Open a chrome browser
 //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
+
 //3- Do not enter any information
 //4- Click to “Sign in” button
-        LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
 
         libraryLoginPage.signInButton.click();
 //5- Verify expected error is displayed:
@@ -35,9 +41,9 @@ public class POMPractice {
 //    TC #2: Invalid email format error message test
 //1- Open a Chrome browser
 //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com ");
+
 //3- Enter invalid email format
-       libraryLoginPage = new LibraryLoginPage();
+
 
         libraryLoginPage.inputUsername.sendKeys("somethingwrong");
 
@@ -56,8 +62,8 @@ public class POMPractice {
 
         //1- Open a chrome browser
             //2- Go to: https://library1.cydeo.com
-            Driver.getDriver().get("https://library1.cydeo.com");
-           libraryLoginPage = new LibraryLoginPage();
+
+
         //3- Enter incorrect username or incorrect password
             Faker faker = new Faker();
             //libraryLoginPage.inputUsername.sendKeys(faker.internet().emailAddress());its working
