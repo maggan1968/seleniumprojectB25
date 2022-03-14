db722c132ab07b4b758b6133e7e24ce774dd2cfe
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-03-14 12:56:14 -0500

    day13  Utilities

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriverWaitPractice.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriverWaitPractice.java
@@ -39,29 +39,31 @@ public class WebDriverWaitPractice {
 
     @Test
     public void dynamic_load_1_test(){
-
-
-    //    TC#5 : Dynamically Loaded Page Elements 1
-    //    1. Go to https://practice.cydeo.com/dynamic_loading/1
-    Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1 ");
+        //1. Go to https://practice.cydeo.com/dynamic_loading/1
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
 
         DynamicLoad1Page dynamicLoad1Page = new DynamicLoad1Page();
-        WebDriverWait wait= new WebDriverWait(Driver.getDriver(), 10);
-    //    2. Click to start
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
+        //2. Click to start
         dynamicLoad1Page.startButton.click();
-    //    3. Wait until loading bar disappears
-       wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
-    //    4. Assert username inputbox is displayed
+
+        //3. Wait until loading bar disappears
+        wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
+
+        //4. Assert username inputbox is displayed
         Assert.assertTrue(dynamicLoad1Page.inputUsername.isDisplayed());
-    //    5. Enter username: tomsmith
+
+        //5. Enter username: tomsmith
         dynamicLoad1Page.inputUsername.sendKeys("tomsmith");
-    //    6. Enter password: incorrectpassword
+
+        //6. Enter password: incorrectpassword
         dynamicLoad1Page.inputPassword.sendKeys("somethingwrong");
-    //    7. Click to Submit button
+
+        //7. Click to Submit button
         dynamicLoad1Page.submitButton.click();
-    //    8. Assert “Your password is invalid!” text is displayed.
+
+        //8. Assert “Your password is invalid!” text is displayed.
         Assert.assertTrue(dynamicLoad1Page.errorMessage.isDisplayed());
-
-
     }
 }
--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,56 +1,75 @@
 package com.cydeo.utilities;
 
+/*
+In this class only general utility methods that are NOT related to some specific page.
+ */
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
-/*
- # 1.In this class only general utility methods that are not related to same specific page.
- */
-     public class BrowserUtils {
-    //1.this method will accept int(in second) and execute Thread.sleep for given duration
+public class BrowserUtils {
 
-    public static void sleep(int second){
-        second*=1000;
+    /*
+   This method will accept int (in seconds) and execute Thread.sleep
+   for given duration
+    */
+    public static void sleep(int second) {
+        second *= 1000;
         try {
             Thread.sleep(second);
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
 
-            }
         }
-   /*
- #  2. This method accepts 3 argument
-   Arg1. webdriver
-   Arg2. expectedInURL: for verify if the url contains given String.
-      - If condition matches, will break loop'\.
-   Arg3. expectedInTitle to be compared against actualTitle
-    */
-    public static void switchWindowAndVerify(WebDriver driver,String expectedInUrl, String expectedTitle ){
+    }
+
+
+    /*
+    This method accepts 3 arguments.
+    Arg1: webdriver
+    Arg2: expectedInUrl : for verify if the url contains given String.
+        - If condition matches, will break loop.
+    Arg3: expectedInTitle to be compared against actualTitle
+     */
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle) {
 
         Set<String> allWindowsHandles = driver.getWindowHandles();
-        for (String each : allWindowsHandles) {
-            driver.switchTo().window(each);
-            System.out.println("Current URL: " + driver.getCurrentUrl()); //stays on Google
 
-            // if statement we don't need if we have only 2 windows
-            if (driver.getCurrentUrl().contains("expectedInUrl")) {
-                break;// stopping and stay on expectedTitle
+        for (String each : allWindowsHandles) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+                break;
             }
         }
 
-        //  Assert: Title contains “expectedTitle”
+        //5. Assert:Title contains “expectedInTitle”
         String actualTitle = driver.getTitle();
-        Assert.assertTrue(actualTitle.contains(expectedTitle));
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+
+    /*
+    This method accepts a String "expectedTitle" and Asserts if it is true
+     */
+    public static void verifyTitle(WebDriver driver, String expectedTitle) {
+
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
 
     }
     /*
-  # 3. This static accepts a String "expectedTitle" and Asserts if it is true
+    Creating a utility method for ExplicitWait,so we don't have to repeat thw lines
      */
-    public static void verifyTitle(WebDriver driver,String expectedTitle){
-
-        Assert.assertEquals(driver.getTitle(), expectedTitle);
+    public static void waitForInvisibilityOf(WebElement webElement){
+        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
     }
 }
-
-
